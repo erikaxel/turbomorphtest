f3c0605b42dd3e0dd3b9419e8f61b3ad5ed5e431
Erik Axel Nielsen <erikaxel@lucalabs.com>
2025-08-24 13:20:16 +0200

    Testing flatpickr and Turbo morph

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,10 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  initialize() {
+    console.log("Initializing flatpickr")
+  }
   connect() {
+    console.log("Connecting flatpickr")
     flatpickr(this.element, {
       dateFormat: "Y-m-d",
       allowInput: true
     })
+  }
+
+  disconnect() {
+    console.log("Disconnected")
   }
 }
